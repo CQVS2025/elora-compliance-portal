5561fd6a12309799a346589da68c92642f5a7397
Jonny Harper <operations@cqvs.com.au>
2025-12-12 06:31:10 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,11 +1,13 @@
 import Dashboard from './pages/Dashboard';
 import MobileDashboard from './pages/MobileDashboard';
+import NotificationSettings from './pages/NotificationSettings';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Dashboard": Dashboard,
     "MobileDashboard": MobileDashboard,
+    "NotificationSettings": NotificationSettings,
 }
 
 export const pagesConfig = {
